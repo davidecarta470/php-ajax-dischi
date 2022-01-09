2edc79eef5ece566ba353c28ec5b336ec4294121
davidecarta470 <cartadavi@gmail.com>
2022-01-09 16:00:34 +0100

    third commit

--- db/index.php
+++ db/index.php
@@ -2,10 +2,22 @@
 require_once __DIR__ . "/db.php";
 $filteredDisk = [];
 $genre = $_GET["genre"];
+$year = $_GET["year"];
+
+
 if(!empty($_GET)){
   $filteredDisk=[];
+  if(!empty($genre))
   foreach($disks as $disk){
     if(array_search($genre,$disk)){
+        array_push($filteredDisk,$disk);
+    }
+  }
+  if(!empty($year))
+  foreach($disks as $disk){
+    
+
+    if(($disk["year"]>(int)$year)&&($disk["year"]<(int)$year+ 10) ){
         array_push($filteredDisk,$disk);
     }
   }
--- script.js
+++ script.js
@@ -3,28 +3,41 @@ var app = new Vue({
   data: {
     apiURL: 'http://localhost/php-ajax-dischi/db/index.php?',
     disks :[],
-    diskGenre : '',
     newUrl : '',
     genreChosed:'',
+    isLoading:true
   },
   methods:{
     getApi(url){
       axios.get(url)
       .then(r=>{
         this.disks = r.data
+        
       })
       .catch(e=>{
         console.log('errore --->>>>> ',e)
       })
+      this.isLoading=false
     },
     getGenre(){
-      console.log(event.originalTarget.value)
       this.genreChosed=''
       this.newUrl = this.apiURL
         if(event.originalTarget.value!=="ALL"){
-          this.diskGenre = event.originalTarget.value
-          this.newUrl=this.apiURL+"genre="+this.diskGenre
-          this.genreChosed='chosed',
+          diskGenre = "genre="+event.originalTarget.value
+          this.newUrl=this.apiURL+diskGenre
+          this.genreChosed='chosed'
+          this.getApi(this.newUrl)
+        }
+         
+        this.getApi(this.newUrl)
+    },
+    getYear(){
+      this.genreChosed=''
+      diskYear=''
+      this.newUrl = this.apiURL
+        if(event.originalTarget.value!=="ALL"){
+          this.diskYear = "year="+event.originalTarget.value
+          this.newUrl=this.apiURL+this.diskYear
           this.getApi(this.newUrl)
         }
          
